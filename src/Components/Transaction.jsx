c8530b2ad81109a301124f3f5000c92ba313bbd5
Caliche Orozco <caliche2da1@gmail.com>
2024-05-12 02:53:58 -0600

    refactor: Improve date formatting in Transaction component

--- src/Components/Transaction.jsx
+++ src/Components/Transaction.jsx
@@ -10,7 +10,15 @@ function Transaction ({
   categories
 }) {
   // eliminar T00:00:00Z de la fecha y convertir a formato legible
-  const date = new Date(datetime).toISOString().split('T')[0]
+  let date = new Date(datetime).toISOString().split('T')[0]
+  // separar por -
+  const dateParts = date.split('-')
+  const year = dateParts[0]
+  const month = dateParts[1]
+  // obtener el nombre del mes
+  const monthName = getMonthName(parseInt(month))
+  const day = dateParts[2]
+  date = `${monthName} ${day} ${year}`
   const priceIndicator = price.slice(0, 1)
   const formattedPrice = `${priceIndicator} $${price.slice(1)}`
   const priceClass = priceIndicator === '+' ? 'text-green-400' : 'text-red-400'
@@ -26,6 +34,12 @@ function Transaction ({
     description.charAt(0).toUpperCase() + description.slice(1).toLowerCase()
 
   category = formattedCategory
+
+  function getMonthName (monthNumber) {
+    const date = new Date(Date.UTC(2020, monthNumber, 1))
+
+    return date.toLocaleString('en-US', { month: 'long' })
+  }
 
   return (
     <div className='transaction w-full flex justify-between py-1 border-b border-custom'>
