b73bec0dc10fdd6a20ccc648a746a66424a17c6e
Caliche Orozco <caliche2da1@gmail.com>
2024-05-18 11:10:36 -0600

    feat: Add categories constant for transaction types

---
+++ src/constants/data.jsx
@@ -0,0 +1,48 @@
+const categories = [
+  { label: '📈 Incomes', value: 'disabled', disabled: true },
+  { label: '💼 Salary', value: 'Salary' },
+  { label: '⏰ Hourly Wages', value: 'Hourly Wages' },
+  { label: '🏢 Business Income', value: 'Business Income' },
+  { label: '🖋️ Freelance Income', value: 'Freelance Income' },
+  { label: '💸 Bonuses and Commissions', value: 'Bonuses and Commissions' },
+  { label: '📊 Investment Income', value: 'Investment Income' },
+  { label: '🏠 Rental Income', value: 'Rental Income' },
+  { label: '🛋️ Retirement Income', value: 'Retirement Income' },
+  { label: '🌴 Passive Income', value: 'Passive Income' },
+  { label: '👟 Side Hustles', value: 'Side Hustles' },
+  { label: '🎁 Gifts Received', value: 'Gifts Received' },
+  { label: '🧾 Tax Refund', value: 'Tax Refund' },
+  {
+    label: '📺 Subscription Reimbursement',
+    value: 'Subscription Reimbursement'
+  },
+  { label: '🔄 Friend Transfers', value: 'Friend Transfers' },
+  { label: '🤲 Loaned Money Received', value: 'Loaned Money Received' },
+
+  { label: '💸 Bills', value: 'disabled', disabled: true },
+  { label: '🏡 Housing', value: 'Housing' },
+  { label: '💡 Utilities', value: 'Utilities' },
+  { label: '🍽️ Food', value: 'Food' },
+  { label: '🚗 Transportation', value: 'Transportation' },
+  { label: '🏥 Medical & Healthcare', value: 'Medical & Healthcare' },
+  { label: '🔒 Insurance', value: 'Insurance' },
+  { label: '💰 Savings & Investments', value: 'Savings & Investments' },
+  { label: '💳 Personal Spending', value: 'Personal Spending' },
+  { label: '🔖 Debts', value: 'Debts' },
+  { label: '🎓 Education & Training', value: 'Education & Training' },
+  { label: '💅 Personal Care', value: 'Personal Care' },
+  { label: '👗 Clothing', value: 'Clothing' },
+  { label: '🎬 Streaming Service', value: 'Streaming Service' },
+  {
+    label: '🎡 Recreation & Entertainment',
+    value: 'Recreation & Entertainment'
+  },
+  { label: '✈️ Travel', value: 'Travel' },
+  { label: '🎁 Gifts & Donations', value: 'Gifts & Donations' },
+  { label: '🐾 Pets', value: 'Pets' },
+  { label: '💸 Money Lent to Friends', value: 'Money Lent to Friends' },
+  { label: '📝 Debts to Friends', value: 'Debts to Friends' },
+  { label: '❓ Other', value: 'Other' }
+]
+
+export default categories
